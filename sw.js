d32c331778b9cfd52cc02163e85c7e22023a8406
gabovernal-lgtm <gabovernal@gmail.com>
2026-09-19 09:59:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='operaciones-ddh-pdf-cronos-prueba-20260919';
+const CACHE='operaciones-ddh-pdf-cronos-fix-20260919b';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
