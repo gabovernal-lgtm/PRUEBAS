6e974662c5c759063b71c20b7f02d34a42fa9739
gabovernal-lgtm <gabovernal@gmail.com>
2026-09-19 14:36:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Exclusivamente proyecto OPERACIONES-DDH-PRUEBAS. Clave PUBLICABLE, nunca secret/service_role.
 const AUTH_PROJECT_URL = "https://pazzvsnifptsfgrsaznx.supabase.co";
-const AUTH_PUBLIC_KEY = "sb_publishable_crfs_zLm0TX3FTJi0QEh-A_VVeVnYKG";
+const AUTH_PUBLIC_KEY = "sb_publishable_crfs_zLmOTX3FTJi0QEh-A_VWeVnYKG";
